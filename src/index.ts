a6a6a31028ff9dfe37f06ceab6c6de54a51d5d51
PoQuatre <leflem.mateo2002@gmail.com>
2022-05-03 10:08:37 +0200

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import { config as dotenv } from "dotenv";
+import express from "express";
+import mongoose from "mongoose";
+import morgan from "morgan";
+
+dotenv();
+
+if (!process.env.MONGO_URL) {
+  throw new Error("The environment variable `MONGO_URL` was not set!");
+}
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+mongoose.connect(process.env.MONGO_URL);
+
+app.use(morgan("common"));
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`The server is now listening on http://localhost:${PORT}`);
+});
